5fffbf6d76d909dd7c941deeac5ae805d462b163
buddy-codes <vodlsreal@gmail.com>
2023-05-27 12:18:35 -0400

    🐛 Replit Link Bug

--- src/lib/Socials.svelte
+++ src/lib/Socials.svelte
@@ -10,7 +10,7 @@
 		<a href="https://twitter.com/bddylol"><Icon width={32} icon="mdi:twitter" color="#fff" /></a>
 		<a href="https://github.com/buddy-codes"><Icon width={32} icon="mdi:github" color="#fff" /></a>
 		<a href="mailto:contact@bddy.lol"><Icon width={32} icon="mdi:email" color="#fff" /></a>
-		<a href="mailto:contact@bddy.lol"><Icon width={32} icon="simple-icons:replit" color="#fff" /></a
+		<a href="https://replit.com/@bddy"><Icon width={32} icon="simple-icons:replit" color="#fff" /></a
 		>
 	</footer>
 </section>
