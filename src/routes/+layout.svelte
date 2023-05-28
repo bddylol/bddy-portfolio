2223acde27586878f6b7c80cacecffff451f56d6
buddy-codes <vodlsreal@gmail.com>
2023-05-27 20:26:32 -0400

    📝 Meta Tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,38 @@
 	});
 </script>
 
+<svelte:head>
+	<!-- HTML Meta Tags -->
+	<title>Avery's Portfolio</title>
+	<meta
+		name="description"
+		content="View some of the technologies and coding languages Avery knows. You can also see more about him here!"
+	/>
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://bddy.lol" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Avery's Portfolio" />
+	<meta
+		property="og:description"
+		content="View some of the technologies and coding languages Avery knows. You can also see more about him here!"
+	/>
+	<meta property="og:image" content="preview.png" />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="bddy.lol" />
+	<meta property="twitter:url" content="https://bddy.lol" />
+	<meta name="twitter:title" content="Avery's Portfolio" />
+	<meta
+		name="twitter:description"
+		content="View some of the technologies and coding languages Avery knows. You can also see more about him here!"
+	/>
+	<meta name="twitter:image" content="preview.png" />
+
+	<!-- Meta Tags Generated via https://www.opengraph.xyz -->
+</svelte:head>
+
 <div
 	class="flex items-center justify-center w-full h-full overflow-auto bg-midnight-600 text-midnight-50"
 >
