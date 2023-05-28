5279065d034d8d14313881960e3c20a3756bd651
buddy-codes <vodlsreal@gmail.com>
2023-05-27 20:21:09 -0400

    🚀 Responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div class="flex items-center content-between justify-between w-3/4 gap-5">
-	<div class="flex flex-col flex-wrap items-center justify-center gap-5">
+	<div class="flex flex-col flex-wrap items-center justify-center w-full gap-5 md:w-fit">
 		<!-- <div class="flex flex-col items-center justify-center">
 			<img src="favicon.png" alt="me as my pfp" class="w-24 h-24 rounded-full" />
 			<h1 class="text-2xl font-bold">Avery</h1>
@@ -19,7 +19,7 @@
 		<!-- <Music /> -->
 		<UserCard />
 	</div>
-	<div class="flex flex-wrap items-center gap-5 h-fit">
+	<div class="flex-wrap items-center hidden gap-5 h-fit md:flex">
 		<CodingLanguage language="LANGUAGE" text="JavaScript & TypeScript" />
 		<CodingLanguage language="LANGUAGE" text="HTML & CSS" />
 		<CodingLanguage language="LIBRARY" text="Tailwind CSS" />
